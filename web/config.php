409d9500599b183f0eb5a1db8a948b5e8d7846aa
TonyZhang <tonyzhang1995@163.com>
2018-03-08 08:28:03 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('APIKEY', 'AIzaSyDeroeAnTxHhpX54U-kZ8bBksa_oBrJH3Q');
+define('GJ_CODE', 'HK');
+define('SITE_NAME', 'YOUTUBE_HEROKU');
+define('TITLENAME', 'YOUTUBE_HEROKU');
+define('EN2DEKEY', 'HMrcJZNVaC');
+define('EMAIL', 'tonyzhang1995@gmail.com');
 ?>
